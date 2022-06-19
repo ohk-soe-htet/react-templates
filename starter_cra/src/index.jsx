998f8116d3cf698adcb1ae6fd7d1c976698d2008
Ohk Soe Htet <ohksoe2002@gmail.com>
2022-06-19 06:50:10 +0630

    updated index.js files

--- starter_cra/src/index.jsx
+++ starter_cra/src/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
@@ -6,15 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-
+const root = createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                <App />
-            </BrowserRouter>
-        </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 );
