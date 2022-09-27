4af7c499403a0a55cee4f6bb40ed75632df7a669
Ohk Soe Htet <ohksoe2002@gmail.com>
2022-09-27 20:25:10 +0630

    vite packages updated

--- vite/src/main.jsx
+++ vite/src/main.jsx
@@ -1,5 +1,5 @@
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
