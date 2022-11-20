869c81f95cf4b9f7ad4df467021db71810c5e425
Ohk Soe Htet <ohksoe2002@gmail.com>
2022-11-20 21:15:04 +0630

    eslint template added

---
+++ cra-lint/src/App.jsx
@@ -0,0 +1,13 @@
+/** @format */
+import React from "react";
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>Nice to meet you</p>
+        </>
+    );
+};
+
+export default App;
