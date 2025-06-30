3b7604ef2ceab71ceb33f6ded881db509cf04f0c
Ohk Soe Htet <ohksoe2002@gmail.com>
2025-06-30 16:36:39 +0800

    update ts config

--- ts/vanilla/eslint.config.js
+++ ts/vanilla/eslint.config.js
@@ -2,38 +2,25 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import typescriptEslintParser from "@typescript-eslint/parser";
-import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
-import { defineConfig, globalIgnores } from "eslint/config";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
 
-export default defineConfig([
+export default tseslint.config([
 	globalIgnores(["dist"]),
 	{
-		files: ["**/*.{js,jsx,ts,tsx}"],
+		files: ["**/*.{ts,tsx}"],
 		extends: [
 			js.configs.recommended,
+			tseslint.configs.recommended,
 			reactHooks.configs["recommended-latest"],
 			reactRefresh.configs.vite,
 		],
 		languageOptions: {
 			ecmaVersion: 2020,
 			globals: globals.browser,
-			parser: typescriptEslintParser,
-			parserOptions: {
-				ecmaVersion: "latest",
-				ecmaFeatures: { jsx: true },
-				sourceType: "module",
-			},
-		},
-		plugins: {
-			"@typescript-eslint": typescriptEslintPlugin,
 		},
 		rules: {
-			"no-unused-vars": "off",
-			"@typescript-eslint/no-unused-vars": [
-				"error",
-				{ varsIgnorePattern: "^[A-Z_]" },
-			],
+			"@typescript-eslint/no-unused-vars": "off",
 		},
 	},
 ]);
